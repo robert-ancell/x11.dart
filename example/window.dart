b57c46ef3a62d2854380992790471ca1e0deeff1
Robert Ancell <robert.ancell@gmail.com>
2020-09-04 07:56:37 +1200

    Add window example

---
+++ example/window.dart
@@ -0,0 +1,23 @@
+import 'package:x11/x11.dart';
+
+void main() async {
+  var client = X11Client();
+  await client.connect();
+
+  client.eventStream.listen((event) {
+    if (event is X11Expose) {
+      client.clearArea(event.window);
+    }
+  });
+
+  var id = client.generateId();
+  client.createWindow(id, client.roots[0].window,
+      width: 400,
+      height: 300,
+      eventMask: {X11EventMask.exposure},
+      backgroundPixel: 0x00000000);
+  var wmNameAtom = await client.internAtom('WM_NAME');
+  var stringAtom = await client.internAtom('STRING');
+  client.changePropertyString(id, wmNameAtom, stringAtom, "x11.dart");
+  client.mapWindow(id);
+}
